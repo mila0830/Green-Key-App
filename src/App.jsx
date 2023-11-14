0d59b7bc95c27b6f6cb1c8074fedb73f5d39aa3e
Mila Rushkin <mila.rushkin@mail.mcgill.ca>
2023-11-13 21:09:53 -0500

    Started the React App!

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+export function InputFileUpload() {
+  return (
+    <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+      Upload file
+      <VisuallyHiddenInput type="file" />
+    </Button>
+  );
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Green Key
+        </a>
+
+      </header>
+      <InputFileUpload/>
+    </div>
+  );
+}
+
+export default App;
